1fb7f03b4ae3ed0f1759f75c577ed52ca1156163
Johan Sannemo <jsannemo@google.com>
2018-04-08 15:04:00 +0200

    Add genall script

---
+++ genall.sh
@@ -0,0 +1,13 @@
+for day in practice day1 day2
+do
+    for problem in $(ls $day)
+    do
+        for lang in de da et fi is lt lv no pl sv en
+        do
+            if [[ -f $day/$problem/problem_statement/problem.$lang.tex ]]
+            then
+                ./gen.sh $day $problem $lang
+            fi
+        done
+    done
+done
