04aad52e036564d816e332d6b144877cdd8c0fa6
Simon Lindholm <simon.lindholm10@gmail.com>
2018-04-16 19:44:51 +0200

    peaks: improved Java helper

--- day1/peaks/attachments/Helper.java
+++ day1/peaks/attachments/Helper.java
@@ -3,34 +3,40 @@ import java.io.*;
 class Grader {
 	public int N, M, K, Q;
 
-	public Grader() throws IOException {
-		String[] ar = new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ");
-		N = Integer.parseInt(ar[0]);
-		M = Integer.parseInt(ar[1]);
-		K = Integer.parseInt(ar[2]);
-		Q = Integer.parseInt(ar[3]);
-		is = new FileInputStream(FileDescriptor.in);
-		os = new FileOutputStream(FileDescriptor.out);
-		buf = new byte[100];
+	public Grader() {
+		try {
+			String[] ar = new BufferedReader(new InputStreamReader(System.in)).readLine().split(" ");
+			N = Integer.parseInt(ar[0]);
+			M = Integer.parseInt(ar[1]);
+			K = Integer.parseInt(ar[2]);
+			Q = Integer.parseInt(ar[3]);
+			is = new FileInputStream(FileDescriptor.in);
+			os = new FileOutputStream(FileDescriptor.out);
+			buf = new byte[100];
+		} catch(Exception e) { throw new RuntimeException(e); }
 	}
 
-	public int query(int x, int y, int z) throws IOException {
-		os.write(("? " + x + " " + y + " " + z + "\n").getBytes());
-		int ret = 0;
-		for (;;) {
-			int count = is.read(buf);
-			if (count == 0 || buf[0] == 45) System.exit(0);
-			for (int i = 0; i < count; i++) {
-				if (buf[i] == 10) return ret;
-				ret *= 10;
-				ret += (int)(buf[i] - 48);
+	public int query(int x, int y, int z) {
+		try {
+			os.write(("? " + x + " " + y + " " + z + "\n").getBytes());
+			int ret = 0;
+			for (;;) {
+				int count = is.read(buf);
+				if (count == 0 || buf[0] == 45) System.exit(0);
+				for (int i = 0; i < count; i++) {
+					if (buf[i] == 10) return ret;
+					ret *= 10;
+					ret += (int)(buf[i] - 48);
+				}
 			}
-		}
+		} catch(Exception e) { throw new RuntimeException(e); }
 	}
 
-	public void guess(int x, int y, int z) throws IOException {
-		os.write(("! " + x + " " + y + " " + z + "\n").getBytes());
-		System.exit(0);
+	public void guess(int x, int y, int z) {
+		try {
+			os.write(("! " + x + " " + y + " " + z + "\n").getBytes());
+			System.exit(0);
+		} catch(Exception e) { throw new RuntimeException(e); }
 	}
 
 	private FileInputStream is;
@@ -40,7 +46,7 @@ class Grader {
 
 
 class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		Grader g = new Grader();
 
 		// TODO do something smart
