08f23f55ed441a734a220d84514f707711a56fbf
Simon Lindholm <simon.lindholm10@gmail.com>
2018-04-18 19:19:31 +0200

    polygon: better test data

--- day1/polygon/data/gen_random.py
+++ day1/polygon/data/gen_random.py
@@ -14,10 +14,12 @@ def cmdlinearg(name, default=None):
 
 random.seed(int(cmdlinearg('seed', sys.argv[-1])))
 n = int(cmdlinearg('n'))
-mode = cmdlinearg('mode', 'normal')
+mode = cmdlinearg('mode', 'uniform')
+mode, *params = mode.split(',')
 
+# generate parent list for a tree rooted in node 0
 def gen_tree(n, mode):
-    eds = [0]    # this makes 0 root, but we could choose some other root
+    par = [0]
     assert mode in ['random', 'star', 'line', 'shallow', 'deep', 'deeper']
     for i in range(1, n):
         if mode == 'random':
@@ -39,29 +41,61 @@ def gen_tree(n, mode):
         else:
             assert False
         assert 0 <= pred < i
-        eds.append(pred)
-    return eds
+        par.append(pred)
+    return par
 
-target = []
-if mode == 'normal':
-    target = [random.randrange(n) for _ in range(n)]
-elif mode == 'perm':
-    target = list(range(n))
-    random.shuffle(target)
-elif mode == 'loops':
+# ~n/5 cycles of length ~5 each
+def small_cycles(n):
+    target = []
     while len(target) < n:
         b = len(target)
-        a = min( random.randrange(1, 10), n-b)
+        a = min(random.randrange(1, 10), n-b)
         target.append(b+a - 1)
         for x in range(b+1, b+a):
             target.append(x-1)
+    return target
+
+# Expected length of longest cycle ~ 0.62n, expected number of cycles ~ log n
+def rand_cycles(n):
+    target = list(range(n))
+    random.shuffle(target)
+    return target
+
+target = []
+if mode == 'uniform':
+    target = [random.randrange(n) for _ in range(n)]
+elif mode == 'perm':
+    target = rand_cycles(n)
+elif mode == 'many-cycles':
+    target = small_cycles(n)
+elif mode == 'one-cycle':
+    target = list(range(1, n)) + [0]
+elif mode == 'ego' or mode == 'general':
+    tree_mode, parts, *params = params
+    parts = int(parts)
+    splittings = [0] + sorted(random.sample(range(1, n), parts-1))
+    for a, b in zip(splittings, splittings[1:] + [n]):
+        # nodes a ... b-1
+        assert 0 <= a < b <= n
+        par = gen_tree(b - a, tree_mode)
+        for x in par:
+            target.append(a + x)
+
+    if mode == 'general':
+        # Bind the individual trees together in cycles
+        mode2, = params
+        if mode2 == 'perm':
+            cyc = rand_cycles(parts)
+        elif mode2 == 'many-cycles':
+            cyc = small_cycles(parts)
+        else:
+            assert False, "unknown mode2 " + mode2
+        split_targ = [splittings[x] for x in cyc]
+        for i in range(len(splittings)):
+            assert target[splittings[i]] == splittings[i]
+            target[splittings[i]] = split_targ[i]
 else:
-    m = int(cmdlinearg('m', 1))
-    s = n // m
-    for i in range(m):
-        eds=gen_tree(s, mode)
-        for x in eds:
-         target.append( x+i*s )
+    assert False, "unknown mode " + mode
 
 assert len(target) == n
 def randname():
