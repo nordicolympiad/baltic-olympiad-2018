37e3cd0b43778f9cf23a6e29b0b0e6b8919d61cc
Simon Lindholm <simon.lindholm10@gmail.com>
2018-05-02 12:37:53 +0200

    Update genall.sh

--- genall.sh
+++ genall.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# TODO: finalize .memlimit and .timelimit files after kattis install
 for day in practice day1 day2
 do
     for problem in $(ls $day)
     do
-        for lang in de da et fi is lt lv no pl sv en
+        for lang in de da et fi is lt lv no pl sv en ru
         do
             if [[ -f $day/$problem/problem_statement/problem.$lang.tex ]]
             then
